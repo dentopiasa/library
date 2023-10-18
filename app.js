fa97f5d3c0480fe60df8e0404d514cd51f9e0387
dentopiasa <castelli_philippe@hotmail.com>
2023-10-18 13:12:00 +0300

    refactoring app.js, working render function

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
 const myLibrary = []; //library array
 
-function Book(title, author, pages) { //constructor 
+function Book(title, author, pages, read) { //constructor 
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
 }
 
-function addBookToLibrary() { //add book function
+//add book function
+function addBookToLibrary() { 
     let title = document.querySelector("#title").value;
     let author = document.querySelector("#author").value;
     let pages = document.querySelector("#pages").value;
     let read = document.querySelector("#read").checked;
     let newBook = new Book(title, author, pages, read);
-    console.log(newBook);
+    myLibrary.push(newBook);
+    render();
 }
 
+//add book button reveals form
 let newBookBtn = document.querySelector("#addNew")
 newBookBtn.addEventListener("click", function() {
     let newBookForm = document.querySelector("#new-book-form")
     newBookForm.style.display = "block";
 })
+
+//submit button triggers addBookLibrary function
+document.querySelector("#new-book-form").addEventListener("submit", function(event){
+    event.preventDefault();
+    addBookToLibrary();
+})
+
+//render function
+function render() {
+    
+}
 
 
